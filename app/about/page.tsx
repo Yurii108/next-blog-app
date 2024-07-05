c4706f42deceb7094d1eea4bac901fec380b9460
Yurii Paraska <paraska108@gmail.com>
2024-07-05 18:57:10 +0300

    feat: add about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,9 @@
-import type { Metadata } from "next";
+import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "About",
+  title: "About | Next App",
 };
 
 export default function About() {
-  return <h1>About</h1>;
+  return <h3>Select subitem</h3>;
 }
