d9dbd32d5e6a139fac27b3b3ac5afa1365d80d46
Yurii Paraska <paraska108@gmail.com>
2024-07-17 14:13:29 +0300

    feat:add data users

---
+++ data/users.ts
@@ -0,0 +1,27 @@
+export const users = [
+    {
+        "name": "Lilah Contreras",
+        "email": "tempor.augue@protonmail.org",
+        "password": "123456"
+    },
+    {
+        "name": "Stone Gates",
+        "email": "posuere.at@hotmail.net",
+        "password": "123456"
+    },
+    {
+        "name": "Jonah Burks",
+        "email": "aliquam.auctor@google.com",
+        "password": "123456"
+    },
+    {
+        "name": "Lucian Murray",
+        "email": "commodo.at.libero@yahoo.couk",
+        "password": "123456"
+    },
+    {
+        "name": "Thaddeus Terry",
+        "email": "neque.nullam.ut@aol.org",
+        "password": "123456"
+    }
+]
